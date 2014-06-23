dc1f85a5bc0ed1c6e86646a805a90a4dcfecf00c
wuschLOR <wuschel@elphenbeinturm.de>
2014-06-23 11:42:29 +0200

    rename again

--- getImgFolder.m
+++ getImgFolder.m
@@ -1,32 +1,7 @@
-function [fileStruct] = getImgFolder (folderStr, fileType)
-%%  [fileStruct] = getImgFolder (folder, filetype)
-%
-%  function to get the names of all files from a subfolder. For exampel all png
-%  files from the folder pic und turns them into a array.
-%  
-%  folderStr   = pic (must be string)
-%  fileType    = png (must be string) '*' if you want all files
-%  fileStruct  =
-%    			4x1 struct array containing the fields:
-% 
-%      			  name
-%      			  date
-%      			  bytes
-%      			  isdir
-%      			  datenum
-%      			  statinfo
-%
-%  History
-%  2014-05-13 written (MG)
-%  ----------------------------------------------------------------------------
+ function [fileStruct] = getImgFolder (folderStr, fileType)
 
+%% dummy function just reffers to readFilesFromFolder 
+ 
+fileStruct = readFilesFromFolder (folderStr, fileType)
 
-
-
-  realFolderStr = ['.' filesep folderStr filesep] % makes a wohle path
-
-  fileStruct = dir([realFolderStr '*.' fileType]); % reads all the files
-
-end%function 
-
-%  --------------------------------------------------------------------------  %
+endfunction
