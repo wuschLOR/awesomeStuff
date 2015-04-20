d6d487a1fac92683fdaea9b850b4db6d53d9e805
MG <wuschel@elphenbeinturm.de>
2015-04-20 21:52:50 +0200

    new allcomb + pathAdd update

---
+++ README.md
@@ -0,0 +1,22 @@
+# awesomeStuff
+
+----------------------------------------------------------------------------
+## WHAT DOES THIS THING DO ?
+support functions for octave with use of the psychtoolbox
+
+----------------------------------------------------------------------------
+## REQUIREMENTS
+
+* GNU Octave, version 3.8.X or higher https://www.gnu.org/software/octave/
+* Psychtoolbox-3                      https://psychtoolbox-3.github.io/overview/
+
+----------------------------------------------------------------------------
+## LICENSE and COPPYRIGHT
+
+* Most of this stuff was written by Michael Groh.
+* If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
+* If you use this for awesome science feel free to cite my (hopefully finished) diploma thesis or the resulting paper (also hopefully). 
+* The Code is licensed under [GPL v.3](./LICENSE) unless marked otherwise. 
+* stuff in the [othergreatminds](./othergreatminds/) folder feature work of others:
+  * [allcomb]() licenced under [BSD License](./othergreatminds/allcomb/license.txt); retreaved [from](https://www.mathworks.com/matlabcentral/fileexchange/10064-allcomb); written by Jos van der Geest
+ 
--- pathAdd.m
+++ pathAdd.m
@@ -4,6 +4,8 @@ function pathAdd
 %  or just happy functions that work whatever folder octave is in right now.
 %  ----------------------------------------------------------------------------
 %  History
+#  2015-04-20 mg  now much more elegant since exclutions work on genpath
+%  2015-04-20 mg  othergreatminds + allcomb
 %  2014-11-25 mg  new paths addet
 %  2014-11-23 mg  raname
 %  2014-06-24 mg  added linebreaks for readability and the butbox folder
@@ -14,11 +16,13 @@ function pathAdd
   fprintf('Now adding the current folder to your octave\n');
   fprintf(['this will be ' currentPath '\n']);
   
-#   addpath (genpath(currentPath , 'git')); % for version higher than 3.2
-  addpath( currentPath);
-  addpath([currentPath filesep 'cedrus']);
-  addpath([currentPath filesep 'dummyfunctions']);
-  addpath([currentPath filesep 'beta']);
+  addpath (genpath(currentPath , ['.git'])); % for version higher than 3.2
+#   addpath( currentPath);
+#   addpath([currentPath filesep 'cedrus']);
+#   addpath([currentPath filesep 'dummyfunctions']);
+#   addpath([currentPath filesep 'beta']);
+#   addpath([currentPath filesep 'othergreatminds'])
+#   addpath([currentPath filesep 'othergreatminds' filesep 'allcomb'])
   savepath;
   
   fprintf(' done :) enjoy!\n');
