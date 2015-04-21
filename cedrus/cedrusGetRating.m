0b3da86185bd177a03923b0e8a499a4970da1b4c
MG <wuschel@elphenbeinturm.de>
2015-04-21 10:57:12 +0200

    upgrade to get cedrus rating

--- cedrus/cedrusGetRating.m
+++ cedrus/cedrusGetRating.m
@@ -1,38 +1,39 @@
-function [pressedButtonTime , pressedButtonValue , pressedButtonStr, pressedButtonTimeRaw ] = cedrusgetRating( handle , till)
-%% [pressedButtonTime , pressedButtonValue , pressedButtonStr  ] = cedrusgetRating( handle , till)
+function [pressedButtonTime , pressedButtonValue , pressedButtonStr, evt ] = cedrusgetRating( handle , till)
+
+## [pressedButtonTime , pressedButtonValue , pressedButtonStr  ] = cedrusgetRating( handle , till)
 ##################################ACHTUNG:######################################
-%  THIS FUNCTION DOES NOT USE THE BUTTONBOX TIMING STUFF CAUSE IT DOESN'T WORK 
-%  POPERLY - IF YOU WANT TO HAVE GOOD RATINGS USE ANOTHER DEVICE!
-%  http://docs.psychtoolbox.org/CedrusResponseBox
-%  https://github.com/kleinerm/Psychtoolbox-3/blob/master/Psychtoolbox/PsychHardware/CedrusResponseBox.m
+#  THIS FUNCTION DOES NOT USE THE BUTTONBOX TIMING STUFF CAUSE IT DOESN'T WORK 
+#  POPERLY - IF YOU WANT TO HAVE GOOD RATINGS USE ANOTHER DEVICE!
+#  http://docs.psychtoolbox.org/CedrusResponseBox
+#  https://github.com/kleinerm/Psychtoolbox-3/blob/master/Psychtoolbox/PsychHardware/CedrusResponseBox.m
 #  why use the timestamp and not some of the timings of evt or the keyboard ?
 #  the function below tells you how many calls for evt your octave can handle
 #  a R500 does this 1800-2300 times a second. You also get can retrieve how 
 #  precise the interaction with the buttonbox is. Besides the timestamp is not 
 #  related to any functionality of the box it lets you estimate the reactiontime
 #  more precise than your keyboard with varies 5ms
+#
 ###############################################################################
-# function callspersec = cedrusRate (handle)
-#   counter=0;
-#   till = GetSecs+10;
-#   do
-#     evt = CedrusResponseBox('GetButtons', handle);
-#     timestampPress = GetSecs;
-#     ++counter
-#   until (~isempty(evt) | timestampPress>= till)
-#   callspersec = counter /10
-###############################################################################
-%  INPUT
-%    handle = cedrus box handler 
-%    till =  default = PTB time stamp + 60 timestampPress
-%
-%  History
-%  2014-11-26 mg  changed timing to GetSecs instead of evt.ptbtime
-%  2014-11-24 mg  rename
-%  2014-06-27 mg  written
-% ----------------------------------------------------------------------------
+#  INPUT
+#    handle = cedrus box handler 
+#    till = PTB timestamp required 
+#           default == PTB time stamp + 60 sec timestampLoop
+
+#  OUTPUT
+#    pressedButtonTime = timestamp when octave recives a button press 
+#    pressedButtonValue = value of the button provided by the CedrusBox
+#    pressedButtonStr = ButtonID provided by the CedrusBox
+#    evt = raw evt stuct provided by CedrusResponseBox('GetButtons', handle)
+#
+#  History
+#  2015-04-21 mg  flip argument added and now the output features evt struct
+#  2014-11-26 mg  changed timing to GetSecs instead of evt.ptbtime
+#  2014-11-24 mg  rename
+#  2014-06-27 mg  written
+##############################################################################
   # first thing to do is geting a timestamp 
   timestampStart=GetSecs
+  
   
   # check if till is present
   if nargin < 2
@@ -45,8 +46,9 @@ function [pressedButtonTime , pressedButtonValue , pressedButtonStr, pressedButt
   #sobald ein butten gedrückt wurde das aufzeichnen 
   do
     evt = CedrusResponseBox('GetButtons', handle);
-    timestampPress = GetSecs;
-  until (~isempty(evt) | timestampPress>= till)
+    timestampLoop = GetSecs;
+  until (~isempty(evt) | timestampLoop>= till)
+  
 
 # evt =
 # {
@@ -61,17 +63,17 @@ function [pressedButtonTime , pressedButtonValue , pressedButtonStr, pressedButt
 # }
 
   
-  if timestampPress<= till
+  if timestampLoop<= till
     # taste is gedrückt worden
 #     pressedButtonTime    = evt.ptbtime;
-    pressedButtonTime    = timestampPress; # <- worst part ever evt.ptbtime is reliable as shit
+    pressedButtonTime    = timestampLoop; # <- worst part ever evt.ptbtime is reliable as shit
     pressedButtonStr     = evt.buttonID;
     pressedButtonValue   = evt.button;
   else
     #keine taste ist gedrückt worden
-    pressedButtonTime    = timestampPress;
+    pressedButtonTime    = timestampLoop;
     pressedButtonStr     = 'FAIL';
-    pressedButtonValue   = 9999;
+    pressedButtonValue   = NaN;
   endif
   
   buttons = 1;
